373d1d7406bcfe707d64664fe01690c36d2df96c
Hilly-man <hilkiahe@gmail.com>
2023-04-17 18:01:58 -0700

    struct-dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a dog's basic info
+ * @name: String
+ * @age: Float
+ * @owner: String
+ * Description: Info dogs in a dog shop
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
