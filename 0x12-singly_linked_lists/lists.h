11d456c8f48866d6b934cd4653c44633feb8e200
Hilly-man <hilkiahe@gmail.com>
2023-04-27 20:38:00 -0700

    Print all Element

---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,27 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdlib.h>
+
+/**
+ *stuct list_t - lined list
+ *@next: points to next node
+ *@str: string
+ *@len: lenth for string
+ *Description: singly linked list
+ */
+
+typedef struct list_t
+{
+	struct list_t *next;
+	char *str;
+	unsigned int len;
+}list_t;
+
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
+
+#endif
