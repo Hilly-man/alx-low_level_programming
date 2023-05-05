a905bbaddb8d9e7082a7010e288577908978f9ac
Hilly-man <hilkiahe@gmail.com>
2023-05-04 22:39:47 -0700

    Betty checked

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -6,20 +6,20 @@
  */
 void print_binary(unsigned long int n)
 {
-    int i;
-    int flag = 0;
+	int i;
+	int flag = 0;
 
-    for (i = 63; i >= 0; i--)
-    {
-        if ((n >> i) & 1)
-        {
-            _putchar('1');
-            flag = 1;
-        }
-        else if (flag)
-            _putchar('0');
-    }
+	for (i = 63; i >= 0; i--)
+	{
+	if ((n >> i) & 1)
+	{
+		_putchar('1');
+		flag = 1;
+	}
+	else if (flag)
+		_putchar('0');
+	}
 
-    if (!flag)
-        _putchar('0');
+	if (!flag)
+		_putchar('0');
 }
