c1ef06fae341563a754194d36489ca3e8f041512
Hilly-man <hilkiahe@gmail.com>
2023-05-04 22:27:20 -0700

    correction

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,16 +11,17 @@ unsigned int binary_to_uint(const char *b)
 {
 	unsigned int dec_val = 0;
 	int i;
+
 	if (b == NULL)
-        return 0;
+		return (0);
 
 
 	for (i = 0; b[i] != '\0'; i++)
 	{
 		if (b[i] < '0' || b[i] > '1')
-		return 0;
+		return (0);
 		dec_val = (dec_val << 1) | (b[i] - '0');
 	}
 
-	return dec_val;
+	return (dec_val);
 }
